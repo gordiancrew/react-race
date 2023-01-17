e76cfbcd355d13ba274ae005913677b21eb4c8ce
Uladzimir Kazantsau <gordiancrew@gmail.com>
2023-01-17 14:43:25 +0300

    init: create template project

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Road from './components/road';
+
+function App() {
+  return (
+    <div className="App">
+      <Road />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/road.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "../styles/road.css";
+import Car from "./car";
+
+function Road() {
+
+    return(
+<React.Fragment>
+    <div className="road-box">
+        <Car />
+        <div className="road-line"></div>
+
+
+    </div> 
+</React.Fragment>
+
+    )
+}
+export default Road;
