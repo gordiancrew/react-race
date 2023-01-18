f245ef31b7e36316f1dd5dd2456e5f233fd7e0dc
Vladimir Kazantsev <gordiancrew@gmail.com>
2023-01-19 00:34:51 +0300

    refactor: featch request

--- src/components/api.tsx
+++ src/components/api.tsx
@@ -1,57 +1,54 @@
-import { SetStateAction, useEffect, useState } from "react";
+import {useEffect, useState } from "react";
 
- export interface IProd {
-  id:string;
+export interface ICar {
+	id: number;
+	name: string;
+	color: string;
 }
 
-export interface IProds{
-  products:IProd[];
+// export interface ICars {
+// 	cars: ICar[];
+// }
+
+function MyGarage() {
+	const [error, setError] = useState(null);
+	const [isLoaded, setIsLoaded] = useState<any>(false);
+	const [items, setItems] = useState<Array<ICar>>([]);
+
+	useEffect(() => {
+		fetch("http://localhost:3000/garage")
+			.then((res) => res.json())
+			.then(
+				(result: ICar[]) => {
+					setIsLoaded(true);
+					setItems(result);
+					console.log("consolelog" + result);
+				},
+
+				(error) => {
+					setIsLoaded(true);
+					setError(error);
+				}
+			);
+	}, []);
+
+	if (error) {
+		// return <div>Error: {error.message}</div> ;
+		return <div>Error: error</div>;
+	} else if (!isLoaded) {
+		return <div>Loading...</div>;
+	} else {
+		console.log("we are here");
+		return (
+			<ul>
+				{items.map((car) => (
+					<li key={car.id}>{car.id}{car.name}{car.color}</li>
+				))}
+			</ul>
+
+		//  <h1>hello</h1>
+		);
+	}
 }
 
-function MyComponent() {
-    const [error, setError] = useState(null);
-    const [isLoaded, setIsLoaded] = useState<any>(false);
-    const [items, setItems] = useState<Array<IProd>>([]);
-  
-    useEffect(() => {
-      fetch("https://dummyjson.com/products?limit=10")
-        .then(res => res.json())
-        .then(
-          (result:IProds) => {
-            setIsLoaded(true);
-             setItems(result.products);
-            console.log("consolelog"+result.products)
-          },
-        
-          (error) => {
-            setIsLoaded(true);
-            setError(error);
-          }
-        )
-    }, [])
-  
-    if (error) {
-      // return <div>Error: {error.message}</div> ;
-      return <div>Error: error</div> ;
-    } else if (!isLoaded) {
-      return <div>Loading...</div>;
-    } else {
-      console.log('we are here')
-      return (
-        <ul>
-          {items.map(product => (
-            <li key={product.id}>
-              {product.id} 
-            </li>
-          ))}
-        </ul>
-
-        //  <h1>{items.toString()}</h1>
-        
-       
-      );
-      
-    }
-  }
-
-  export default MyComponent;
+export default MyGarage;
--- src/components/road.tsx
+++ src/components/road.tsx
@@ -3,17 +3,13 @@ import "../styles/road.css";
 import Car from "./car";
 
 function Road() {
-
-    return(
-<React.Fragment>
-    <div className="road-box">
-        <Car />
-        <div className="road-line"></div>
-
-
-    </div> 
-</React.Fragment>
-
-    )
+	return (
+		<React.Fragment>
+			<div className="road-box">
+				<Car />
+				<div className="road-line"></div>
+			</div>
+		</React.Fragment>
+	);
 }
 export default Road;
