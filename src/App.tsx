ae48fe03cea323ba5592b1bcf0a9e65b1b5d0460
Uladzimir Kazantsau <gordiancrew@gmail.com>
2023-01-19 20:59:37 +0300

    feat: header

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,21 @@
 
+
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
 
 import MyGarage from './components/api';
+import Header from './components/header';
 
 function App() {
   return (
-    <div className="App">
-      <MyGarage />
+    <div >
+
+      <Routes>
+        <Route path="/" element={<MyGarage />} />
+        <Route path="/winners" element={<Header />} />
+      </Routes>
     </div>
+
   );
 }
 
---
+++ src/components/header.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "../styles/header.css";
+
+function Header(){
+
+    return(
+<header className="header-box">
+    <button className="header-button">TO GARAGE</button>
+    <button className="header-button">TO WINNERS</button>
+
+
+</header>);
+}
+export default Header;
